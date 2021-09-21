020743663ef1331d2822c4144c8254d0c43855eb
Aleksandrs.1 <jels417@gmail.com>
2021-09-21 15:28:16 +0300

    3uzd

---
+++ 3.uzd.py
@@ -0,0 +1,13 @@
+# Lietotājs ievada veselu pozitīvu skaitli.
+# Izdrukā visus skaitļus sākot no ievadītā līdz 0 (neieskaitot)
+
+#input
+#while cikls
+# >
+# -=
+
+a = int(input("Ievadi veselu skaitli"))
+
+while (a>0):
+    a-=1
+    print(a)
